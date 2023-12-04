62a7293804bf0e1eb1c3c6d1d802a3ce704c3491
Julio Fuentes <patchitodev@gmail.com>
2023-12-04 17:25:37 -0600

    'First commit -- Added the completed project (not ready for production)'

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { useState, Fragment } from "react";
+
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox value={manufacturer} onChange={setManufacturer}>
+        <div className="relative w-full">
+          {/* Button for the combobox, click on the icon to see the complete dropdown */}
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+
+          {/* Input field for searching */}
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)} // Update the search query when the input changes
+          />
+
+          {/* Transition for displaying the options */}
+          <Transition
+            as={Fragment} // Group multiple elements without introducing an additional DOM node
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")} // Reset the search query after the trasnsition completes
+          >
+            <Combobox.Options>
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) =>
+                    `realtive search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+
+                      {/* Show an active blue background color if the option is selected */}
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
---
+++ components/ShowMore.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import { CustomButton } from ".";
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+    // Update the 'limit' search parameter in the URL with the new value
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName, { scroll: false });
+  };
+
+  return (
+    <div className="w-full flex-center gap5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
